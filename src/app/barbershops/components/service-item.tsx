a019285506bbcf958a0fc944cd91a119696fd816
guicarsiqsantos <guicarsiqsantos@gmail.com>
2024-05-16 11:29:37 -0300

    Adicionando o calendario ao tentar reservar um serviço

--- src/app/barbershops/components/service-item.tsx
+++ src/app/barbershops/components/service-item.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent } from "@/components/ui/card";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Service } from "@prisma/client";
+import { ptBR } from "date-fns/locale";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ServiceItemProps {
   service: Service;
@@ -12,6 +22,8 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   const handleBookingClick = () => {
     if (!isAuthenticated) {
       return signIn("google");
@@ -44,9 +56,40 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                   currency: "BRL",
                 }).format(Number(service.price))}
               </p>
-              <Button variant="secondary" onClick={handleBookingClick}>
-                Reservar
-              </Button>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant="secondary" onClick={handleBookingClick}>
+                    Reservar
+                  </Button>
+                </SheetTrigger>
+
+                <SheetContent className="p-0">
+                  <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
+                    <SheetTitle>Fazer Reserva</SheetTitle>
+                  </SheetHeader>
+
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    locale={ptBR}
+                    className="mt-4"
+                    fromDate={new Date()}
+                    styles={{
+                      head_cell: {
+                        width: "100%",
+                        textTransform: "capitalize",
+                      },
+                      cell: {
+                        width: "100%",
+                      },
+                      caption: {
+                        textTransform: "capitalize",
+                      },
+                    }}
+                  />
+                </SheetContent>
+              </Sheet>
             </div>
           </div>
         </div>
